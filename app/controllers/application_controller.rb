60bd9e7715eabf1c2640147d865af912e72a005b
Naomi Dushay <ndushay@stanford.edu>
2011-06-15 18:23:41 -0700

    app controller and helper need to use hydra-head plugin, not blacklight

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require_dependency( 'vendor/plugins/blacklight/app/controllers/application_controller.rb')
+require_dependency( 'vendor/plugins/hydra-head/app/controllers/application_controller.rb')
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
+require 'vendor/plugins/hydra-head/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
