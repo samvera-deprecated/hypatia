246e9464f154eda6327042a278c887768a68cca1
Naomi Dushay <ndushay@stanford.edu>
2011-06-16 16:57:49 -0700

    added helper method for displaying

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     'Hypatia'
   end
   
+  # display the Solr values populated per the datastream model within a fieldset html tag
+  def display_ds_values_as_fieldset(dsid, solr_fld_sym, display_label)
+    values = get_values_from_datastream(@document_fedora, dsid, [solr_fld_sym])
+    unless values.first.empty?
+      result = "<fieldset><legend>#{display_label}</legend><div class=\"browse_value\">#{values.join(', ')}</div></fieldset>"
+    end
+    result 
+  end
+  
 end
