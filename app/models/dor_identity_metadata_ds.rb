ac94c86b31c5b08d40e6a1d20434e5d7422d7e37
Naomi Dushay <ndushay@stanford.edu>
2011-06-22 18:04:18 -0700

    all technical metadata now displays for xanadu drive 1 fixture

--- app/models/dor_identity_metadata_ds.rb
+++ app/models/dor_identity_metadata_ds.rb
@@ -7,14 +7,14 @@ class DorIdentityMetadataDS < ActiveFedora::NokogiriDatastream
     # in alphabetical order
     t.adminPolicy(:index_as=>[:searchable, :displayable, :facetable, :sortable], :required=>:false, :type=>:string, :namespace_prefix => nil )
     t.agreementId(:index_as=>[:searchable, :displayable, :facetable, :sortable],  :required=>:false, :type=>:string, :namespace_prefix => nil )  
+    t.hypatiaId(:index_as=>[:searchable,  :displayable, :facetable, :sortable], :path=>"sourceId", :attributes=>{:source=>"hypatia"},  :required=>:true, :type=>:string, :namespace_prefix => nil )  
     t.objectCreator(:index_as=>[:searchable, :displayable, :facetable, :sortable], :required=>:true, :type=>:string, :namespace_prefix => nil )
     t.objectId(:index_as=>[:searchable, :displayable, :sortable],  :required=>:true, :type=>:string, :namespace_prefix => nil )
     t.objectLabel(:index_as=>[:searchable, :displayable, :facetable, :sortable],  :required=>:true, :type=>:string, :namespace_prefix => nil )  
     t.objectType(:index_as=>[:searchable, :displayable, :facetable, :sortable],  :required=>:true, :type=>:string, :namespace_prefix => nil )  
-    t.otherId(:index_as=>[:searchable,  :displayable, :facetable, :sortable], :attributes=>{:type=>"name"},  :required=>:true, :type=>:string, :namespace_prefix => nil )  
     t.setType(:index_as=>[:searchable, :displayable, :facetable, :sortable], :required=>:false, :type=>:string, :namespace_prefix => nil )
-    t.sourceId(:index_as=>[:searchable,  :displayable, :facetable, :sortable], :attributes=>{:type=>"source"},  :required=>:true, :type=>:string, :namespace_prefix => nil )  
     t.tag(:index_as=>[:searchable, :displayable, :facetable, :sortable],  :required=>:true, :type=>:string, :namespace_prefix => nil )  
+    t.uuid(:index_as=>[:searchable,  :displayable, :facetable, :sortable], :path=>"otherId", :attributes=>{:name=>"uuid"},  :required=>:true, :type=>:string, :namespace_prefix => nil )  
   end
   
 end #class
