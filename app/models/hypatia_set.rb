d66faf1df3b72450235c749e4885e51e312dbff8
Naomi Dushay <ndushay@stanford.edu>
2011-07-18 18:13:14 -0700

    set members can be anything, not just xanadu items

--- app/models/hypatia_set.rb
+++ app/models/hypatia_set.rb
@@ -4,6 +4,5 @@ class HypatiaSet < ActiveFedora::Base
   has_metadata :name => "identityMetadata", :type=> DorIdentityMetadataDS
   has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata
   
-  # note that sets could have other sets as members ...
-  has_relationship "members", :is_member_of, :inbound=>true, :type=>HypatiaXanaduItem
+  has_relationship "members", :is_member_of, :inbound=>true
 end
