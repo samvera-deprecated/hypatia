8dfaaa3c975624dd83344a52024d3cd81f468d5c
Naomi Dushay <ndushay@stanford.edu>
2011-07-18 18:12:52 -0700

    add specs for object models, including testing for relationships

---
+++ spec/models/hypatia_set_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "active_fedora"
+
+describe HypatiaSet do
+  
+  before(:each) do
+    Fedora::Repository.stubs(:instance).returns(stub('stub').as_null_object)
+    @hypatia_set = HypatiaSet.new
+  end
+  
+  it "should be a kind of ActiveFedora::Base" do
+    @hypatia_set.should be_kind_of(ActiveFedora::Base)
+  end
+  
+  it "should have a descMetadata datastream of type HypatiaSetDescMetadataDS" do
+    @hypatia_set.datastreams.should have_key("descMetadata")
+    @hypatia_set.datastreams["descMetadata"].should be_instance_of(HypatiaSetDescMetadataDS)
+  end
+
+  it "should have a identityMetadata datastream of type DorIdentityMetadataDS" do
+    @hypatia_set.datastreams.should have_key("identityMetadata")
+    @hypatia_set.datastreams["identityMetadata"].should be_instance_of(DorIdentityMetadataDS)
+  end
+
+  it "should have a rightsMetadata datastream of type Hydra::RightsMetadata" do
+    @hypatia_set.datastreams.should have_key("rightsMetadata")
+    @hypatia_set.datastreams["rightsMetadata"].should be_instance_of(Hydra::RightsMetadata)
+  end
+    
+  it "should have a members relationship" do
+    @hypatia_set.should respond_to(:members)
+  end
+  
+end
---
+++ spec/models/hypatia_xanadu_item_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "active_fedora"
+
+describe HypatiaXanaduItem do
+  
+  before(:each) do
+    Fedora::Repository.stubs(:instance).returns(stub('stub').as_null_object)
+    @hypatia_xanadu_item = HypatiaXanaduItem.new
+  end
+  
+  it "should be a kind of ActiveFedora::Base" do
+    @hypatia_xanadu_item.should be_kind_of(ActiveFedora::Base)
+  end
+  
+  it "should have a descMetadata datastream of type HypatiaItemDescMetadataDS" do
+    @hypatia_xanadu_item.datastreams.should have_key("descMetadata")
+    @hypatia_xanadu_item.datastreams["descMetadata"].should be_instance_of(HypatiaItemDescMetadataDS)
+  end
+
+  it "should have a contentMetadata datastream of type HypatiaItemContentMetadataDS" do
+    @hypatia_xanadu_item.datastreams.should have_key("contentMetadata")
+    @hypatia_xanadu_item.datastreams["contentMetadata"].should be_instance_of(HypatiaItemContentMetadataDS)
+  end
+
+  it "should have a identityMetadata datastream of type DorIdentityMetadataDS" do
+    @hypatia_xanadu_item.datastreams.should have_key("identityMetadata")
+    @hypatia_xanadu_item.datastreams["identityMetadata"].should be_instance_of(DorIdentityMetadataDS)
+  end
+
+  it "should have a rightsMetadata datastream of type Hydra::RightsMetadata" do
+    @hypatia_xanadu_item.datastreams.should have_key("rightsMetadata")
+    @hypatia_xanadu_item.datastreams["rightsMetadata"].should be_instance_of(Hydra::RightsMetadata)
+  end
+    
+  it "should have a sets relationship" do
+    @hypatia_xanadu_item.should respond_to(:sets)
+  end
+  
+end
