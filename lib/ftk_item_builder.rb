bb82e16327ab00d8d4d2236e6e7471eed5c33533
Bess Sadler <bess@stanford.edu>
2011-09-04 10:41:53 -0700

    Switched out ftk datastreams and it still works

--- lib/ftk_item_builder.rb
+++ lib/ftk_item_builder.rb
@@ -2,24 +2,23 @@ require 'active-fedora'
 require File.join(File.dirname(__FILE__), "/../config/environment.rb")
 require File.join(File.dirname(__FILE__), "/../app/models/hypatia_xanadu_item")
 require File.join(File.dirname(__FILE__), "/../app/models/hypatia_item_desc_metadata_ds")
-# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "/../../vendor/plugins/hydra-head/lib/hydra"))
 
 class FtkItemBuilder
   def initialize()
     ActiveFedora.init
     puts Fedora::Repository.instance.fedora_version
     
-    f = HypatiaXanaduItem.new
-    f.save
-    puts "saved as #{f.pid}"
-    desc = f.datastreams['descMetadata']
+    @f = HypatiaXanaduItem.new
+    @f.save
+    puts "saved as #{@f.pid}"
+    desc = @f.datastreams['descMetadata']
     puts desc.class
     desc.content = descMetadata
     desc.ng_xml = Nokogiri::XML::Document.parse(descMetadata)
     desc.dirty = true
     desc.save
     
-    content = f.datastreams['contentMetadata']
+    content = @f.datastreams['contentMetadata']
     puts content.class
     content.content = contentMetadata
     content.ng_xml = Nokogiri::XML::Document.parse(contentMetadata)
@@ -33,30 +32,31 @@ class FtkItemBuilder
     # rights.dirty = true
     # rights.save
     
-    id = f.datastreams['identityMetadata']
+    id = @f.datastreams['identityMetadata']
     puts id.class
     id.content = identityMetadata
     id.ng_xml = Nokogiri::XML::Document.parse(identityMetadata)
     id.dirty = true
     id.save
     
-    f.datastreams["rightsMetadata"].permissions({:group=>"public"}, "read")
-    f.datastreams["rightsMetadata"].permissions({:group=>"public"}, "discover")
-    f.save
-    puts "obj saved as #{f.pid}"
+    @f.datastreams["rightsMetadata"].permissions({:group=>"public"}, "read")
+    @f.datastreams["rightsMetadata"].permissions({:group=>"public"}, "discover")
+    @f.save
+    puts "obj saved as #{@f.pid}"
     
   end
   
   def contentMetadata
-    '<contentMetadata type="born-digital" objectId="druid:tk694zs2244">
-      <resource id="analysis-text" type="analysis" data="metadata" objectId="druid:nd615pr9748">
-        <file id="item1.txt" format="TEXT" mimetype="text/plain" encoding="UTF-8" size="1250" preserve="yes" shelve="yes" deliver="yes">
-          <location type="url">http://stacks.stanford.edu/file/druid:nd615pr9748/item1.txt</location>
-          <checksum type="md5">37cf7f100f80d4088ca011ad977a154b</checksum>
-          <checksum type="sha1">a25bc23d85ac1d584bfdce4976149842e8280ad6</checksum>
+    "<?xml version='1.0'?>
+    <contentMetadata type='born-digital' objectId='foofile.txt_9999'>
+      <resource data='metadata' id='analysis-text' type='analysis' objectId='#{@f.pid}'>
+        <file size='504 B' format='WordPerfect 5.1' id='foofile.txt'>
+          <location type='filesystem'>files/foofile.txt</location>
+          <checksum type='md5'>4E1AA0E78D99191F4698EEC437569D23</checksum>
+          <checksum type='sha1'>B6373D02F3FD10E7E1AA0E3B3AE3205D6FB2541C</checksum>
         </file>
       </resource>
-    </contentMetadata>'
+    </contentMetadata>"
   end
   
 
