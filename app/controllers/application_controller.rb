0ff7a17ad00600c614972958a9962d3cde5c2aa3
Jessie Keck <jessie.keck@gmail.com>
2011-09-09 16:40:59 -0500

    Adding in css from AppController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ require_dependency( 'vendor/plugins/hydra-head/app/controllers/application_contr
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  before_filter :inject_assets
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  protected
+  
+  def inject_assets
+    stylesheet_links << ["http://fonts.googleapis.com/css?family=Copse|Open+Sans:300,400,600|Lato:700", "hypatia", {:media=>"all"}]
+  end
 end
