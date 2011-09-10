c739ea349a47593c1219092eac9fcceeaab1da87
Bess Sadler <bess@stanford.edu>
2011-09-10 11:16:56 -0500

    Added scaffolding for FTK disk image item objects

---
+++ app/models/hypatia_disk_image_item.rb
@@ -0,0 +1,8 @@
+# a Fedora object for the Hypatia Xanadu ITEM hydra content type
+class HypatiaDiskImageItem < ActiveFedora::Base
+  has_metadata :name => "descMetadata", :type=> HypatiaItemDescMetadataDS
+  has_metadata :name => "contentMetadata", :type=> HypatiaItemContentMetadataDS
+  has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata
+  has_relationship "members", :is_member_of, :inbound=>true
+  has_relationship "sets", :is_member_of, :type => HypatiaSet
+end
---
+++ lib/ftk_disk_image_item_assembler.rb
@@ -0,0 +1,43 @@
+require "rubygems"
+require "active-fedora"
+
+class FtkDiskImageItemAssembler 
+  
+  attr_accessor :disk_image_files_dir   # The directory containing the disk images
+  attr_reader :filehash # The hash were we store the files we're processing
+  
+  def initialize(args)
+    raise "Can't find directory #{args[:disk_image_files_dir]}" unless File.directory? args[:disk_image_files_dir]
+    @disk_image_files_dir = args[:disk_image_files_dir]
+    @filehash = {}
+    build_file_hash
+  end
+  
+  # Read in all the files in @disk_image_files_dir.
+  # Determine which of these are dd files (the actual disk image),
+  # and which are .csv and .txt metadata about the disk image.
+  # Load these into a hash for easy access as we're building the fedora objects.
+  # We assume that there are only three files for each disk image: 
+  # .txt, .csv, and the disk image, which may end in .dd or not
+  # @return [Hash]
+  def build_file_hash
+    Dir["#{@disk_image_files_dir}/*"].each { |file|
+      disk_number = file.split('/').last.split('.').first
+      
+      # If filehash doesn't have a space for this disk number yet, make one
+      if @filehash[disk_number.to_sym] == nil
+        @filehash[disk_number.to_sym] = {}
+      end
+      file_extension = file.split('/').last.split('.').last
+      if file_extension == 'csv' 
+        @filehash[disk_number.to_sym][:csv] = file
+      elsif file_extension == 'txt' 
+        @filehash[disk_number.to_sym][:txt] = file
+      else
+        @filehash[disk_number.to_sym][:dd] = file
+      end
+    }
+  end
+  
+  
+end
