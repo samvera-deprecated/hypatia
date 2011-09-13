727379e875946a5e513d75594e116a214d9bb9de
Bess Sadler <bess@stanford.edu>
2011-09-12 21:42:42 -0700

    Renamed HypatiaXanaduItem to HypatiaItem so it will be more generic

--- app/models/hypatia_item.rb
+++ app/models/hypatia_item.rb
@@ -1,5 +1,5 @@
-# a Fedora object for the Hypatia Xanadu ITEM hydra content type
-class HypatiaXanaduItem < ActiveFedora::Base
+# a Fedora object for the Hypatia ITEM hydra content type
+class HypatiaItem < ActiveFedora::Base
   has_metadata :name => "descMetadata", :type=> HypatiaItemDescMetadataDS
   has_metadata :name => "contentMetadata", :type=> HypatiaItemContentMetadataDS
   has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata
