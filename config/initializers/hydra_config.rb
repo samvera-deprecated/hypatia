8a0c0b26e4c7faf6ff42713e24879f06a362e5d7
Jessie Keck <jessie.keck@gmail.com>
2011-09-19 15:17:09 -0700

    Adding in preliminary submission workflow configuration and edit partials.

--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -23,17 +23,10 @@ if Hydra.respond_to?(:configure)
       }
     }
     config[:submission_workflow] = {
-        :mods_assets =>      [{:name => "contributor",     :edit_partial => "mods_assets/contributor_form",     :show_partial => "mods_assets/show_contributors"},
-                              {:name => "publication",     :edit_partial => "mods_assets/publication_form",     :show_partial => "mods_assets/show_publication"},
-                              {:name => "additional_info", :edit_partial => "mods_assets/additional_info_form", :show_partial => "mods_assets/show_additional_info"},
-                              {:name => "files",           :edit_partial => "file_assets/file_assets_form",     :show_partial => "mods_assets/show_file_assets"},
-                              {:name => "permissions",     :edit_partial => "permissions/permissions_form",     :show_partial => "mods_assets/show_permissions"}
-                             ],
-        # Not being used right now
-        :generic_contents => [{:name => "description", :edit_partial => "generic_content_objects/description_form", :show_partial => "generic_contents/show_description"},
-                              {:name => "files",       :edit_partial => "file_assets/file_assets_form",             :show_partial => "file_assets/index"},
-                              {:name => "permissions", :edit_partial => "permissions/permissions_form",             :show_partial => "generic_contents/show_permissions"},
-                              {:name => "contributor", :edit_partial => "generic_content_objects/contributor_form", :show_partial => "generic_contents/show_contributors"}
+        :hypatia_items =>    [{:name => "description",    :edit_partial => "hypatia_items/description_form", :show_partial => "hypatia_items/show_description"},
+                              {:name => "files",          :edit_partial => "file_assets/file_assets_form", :show_partial => "shared/show_files"},
+                              {:name => "technical_info", :edit_partial => "hypatia_items/tech_info_form", :show_partial => "shared/show_technical"}#,
+#                              {:name => "permissions",    :edit_partial => "permssions/permissions_form", :show_partial => ""}
                              ]
       }
   end
