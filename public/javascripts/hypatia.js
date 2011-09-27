19290f78876b65760c54f9d423b5ca75309cf948
Jessie Keck <jessie.keck@gmail.com>
2011-09-27 15:31:24 -0700

    Tweaking how we do toggling to make it degradable.

--- public/javascripts/hypatia.js
+++ public/javascripts/hypatia.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+	$(".start-shut").each(function(){
+		$(this).toggle();
+	});
 	$(".show-hide").each(function(){
 		var toggle_link = $(this);
+		toggle_link.toggle();
 		toggle_link.click(function(){
 			toggle_link.next(".toggle-section").slideToggle();
 			toggle_text = (toggle_link.text() == "Hide Section") ? "Show Section" : "Hide Section";
