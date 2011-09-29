0422cc0efd84e443335bdd8cc2e3b75dfe235e1f
Naomi Dushay <ndushay@stanford.edu>
2011-09-29 15:37:12 -0700

    add model for disk-image item desc metadata

---
+++ app/models/hypatia_disk_img_desc_metadata_ds.rb
@@ -0,0 +1,25 @@
+# defines the OM terminology for a Hypatia DISK-IMAGE ITEM object's 
+#  descMetadata datastream, which will have Mods XML.
+class HypatiaDiskImgDescMetadataDS < ActiveFedora::NokogiriDatastream 
+   
+# TODO: what should really be searchable, facetable, displayable, sortable?
+
+  set_terminology do |t|
+    t.root(:path=>"mods", :xmlns=>"http://www.loc.gov/mods/v3", :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-3.xsd")
+
+    t.title_info(:path=>"titleInfo") {
+      t.title(:path=>"title", :index_as=>[:searchable, :displayable, :sortable], :label=>"title")
+    }
+    t.title(:proxy=>[:title_info, :title])
+    
+    t.local_id(:path=>"identifier", :attributes=>{:type=>"local"}, :index_as=>[:searchable, :displayable, :sortable])
+
+    t.physical_desc(:path=>"physicalDescription") {
+      t.extent(:path=>"extent", :index_as=>[:searchable])
+      t.digital_origin(:path=>"digitalOrigin", :index_as=>[:searchable])
+    }
+    t.extent(:proxy=>[:physical_desc, :extent])
+    t.digital_origin(:proxy=>[:physical_desc, :digital_origin])
+  end 
+
+end
---
+++ spec/models/disk_image_desc_md_ds_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe HypatiaDiskImgDescMetadataDS do
+  before(:all) do
+    @desc_md_ds = HypatiaDiskImgDescMetadataDS.from_xml(active_fedora_fixture("disk_image_desc_metadata.xml"))
+  end
+    
+  it "should have the correct :title term value" do
+    @desc_md_ds.term_values(:title).should == ["CM058"]
+    @desc_md_ds.term_values(:title_info, :title).should == ["CM058"]
+  end
+  
+  it "should have the correct :local_id term value" do
+    @desc_md_ds.term_values(:local_id).should == ["CM058"]
+  end
+
+  it "should have the correct :extent term value" do
+    @desc_md_ds.term_values(:extent).should == ["3.5 inch Floppy Disk"]
+  end
+
+  it "should have the correct :digital_origin term value" do
+    @desc_md_ds.term_values(:digital_origin).should == ["Born Digital"]
+  end
+
+end
