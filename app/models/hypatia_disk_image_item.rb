620fe48f83642becc34c913ac7aa48770b99635b
Naomi Dushay <ndushay@stanford.edu>
2011-09-29 14:18:10 -0700

    added question about whether relationships needed to be bidirectional

--- app/models/hypatia_disk_image_item.rb
+++ app/models/hypatia_disk_image_item.rb
@@ -6,11 +6,11 @@ class HypatiaDiskImageItem < ActiveFedora::Base
 
   # a disk image item is a member of a collection
   has_relationship "collections", :is_member_of_collection, :type => HypatiaCollection
-  # a disk image can belong to sets
+  # a disk image can belong to sets  -- FIXME:  does this need to be bidirectional?
 #  has_relationship "sets", :is_member_of, :type => HypatiaSet
   has_bidirectional_relationship "sets", :is_member_of, :has_member
   # a disk image item can have members (e.g.  file items)
   has_relationship "members", :is_member_of, :inbound=>true
-  # there are files such as dd and image to be attached
+  # there are files such as dd and image to be attached  -- FIXME:  does this need to be bidirectional?
   has_bidirectional_relationship "part_of", :is_part_of, :has_part
 end
