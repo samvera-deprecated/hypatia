4cb7e4c8f28e967c16e1e75bcec60bb105ddf301
Naomi Dushay <ndushay@stanford.edu>
2011-10-03 14:57:44 -0700

    added descMetadata model for ftk items

---
+++ app/models/hypatia_ftk_item_desc_metadata_ds.rb
@@ -0,0 +1,44 @@
+# defines the OM terminology for a Hypatia FTK ITEM object's 
+#  descMetadata datastream, which will have Mods XML.
+class HypatiaFTKItemDescMetadataDS < ActiveFedora::NokogiriDatastream 
+   
+# TODO: what should really be searchable, facetable, displayable, sortable?
+
+  set_terminology do |t|
+    t.root(:path=>"mods", :xmlns=>"http://www.loc.gov/mods/v3", :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-3.xsd")
+
+    t.filename(:path=>"identifier", :attributes=>{:type=>"filename"}, :index_as=>[:searchable, :displayable, :sortable])
+    t.display_name(:proxy=>[:filename], :index_as=>[:searchable, :displayable])
+    
+    t.ftk_id(:path=>"identifier", :attributes=>{:type=>"ftk_id"}, :index_as=>[:searchable, :displayable, :sortable])
+
+    t.location(:path=>"location") {
+      t.filepath(:path=>"physicalLocation", :attributes=>{:type=>"filepath"}, :index_as=>[:displayable])
+    }
+    t.filepath(:proxy=>[:location, :filepath], :index_as=>[:displayable])
+    
+    t.filetype(:path=>"note", :attributes=>{:displayLabel=>"filetype"}, :index_as=>[:searchable, :displayable, :facetable])
+
+    t.physical_desc(:path=>"physicalDescription") {
+      t.extent(:path=>"extent", :index_as=>[:displayable])
+    }
+    t.extent(:proxy=>[:physical_desc, :extent])
+
+    t.origin_info(:path=>"originInfo") {
+      t.date_created(:path=>"dateCreated", :index_as=>[:displayable, :sortable])
+      t.date_last_accessed(:path=>"dateOther", :attributes=>{:type=>"last_accessed"}, :index_as=>[:displayable, :sortable])
+      t.date_last_modified(:path=>"dateOther", :attributes=>{:type=>"last_modified"}, :index_as=>[:displayable, :sortable])
+    }
+    t.date_created(:proxy=>[:origin_info, :date_created])
+    t.date_last_accessed(:proxy=>[:origin_info, :date_last_accessed])
+    t.date_last_modified(:proxy=>[:origin_info, :date_last_modified])
+
+    t.related_item(:path=>"relatedItem", :attributes=>{:displayLabel=>"Appears in"}) {
+      t.title_info(:path=>"titleInfo") {
+        t.title(:path=>"title", :index_as=>[:searchable, :displayable, :sortable], :label=>"title")
+      }
+    }
+    t.addl_title(:proxy=>[:related_item, :title_info, :title], :index_as=>[:searchable, :displayable])
+  end 
+
+end
---
+++ spec/models/ftk_item_desc_md_ds_spec.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe HypatiaFTKItemDescMetadataDS do
+  before(:all) do
+    @desc_md_ds = HypatiaFTKItemDescMetadataDS.from_xml(active_fedora_fixture("ftk_item_desc_metadata.xml"))
+  end
+    
+  it "should have the correct :filename value" do
+    @desc_md_ds.term_values(:filename).should == ["BU3A5"]
+  end
+
+  it "should have the correct :display_name value" do
+    @desc_md_ds.term_values(:display_name).should == ["BU3A5"]
+  end
+  
+  it "should have the correct :ftk_id value" do
+    @desc_md_ds.term_values(:ftk_id).should == ["1004"]
+    # should it also have a proxy of :local_id?
+  end
+
+  it "should have the correct :filepath value" do
+    @desc_md_ds.term_values(:filepath).should == ["CM006.001/NONAME [FAT12]/[root]/BU3A5"]
+  end
+
+  it "should have the correct :extent value" do
+    @desc_md_ds.term_values(:extent).should == ["35654"]
+  end
+
+  it "should have the correct :filetype value" do
+    @desc_md_ds.term_values(:filetype).should == ["WordPerfect 4.2"]
+  end
+
+  it "should have the correct date values" do
+    @desc_md_ds.term_values(:date_created).should == ["12/6/1988"]
+    @desc_md_ds.term_values(:date_last_accessed).should == ["12/10/1988"]
+    @desc_md_ds.term_values(:date_last_modified).should == ["12/8/1988 6:48:48 AM (1988-12-08 14:48:48 UTC)"]
+  end
+  
+  it "should have the correct :addl_title value" do
+    @desc_md_ds.term_values(:addl_title).should == ["The Burgess Shale and the Nature of History"]
+  end
+
+end
