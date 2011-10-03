ec9d29ca629a2dadf7ada013bbc8f62a4f6b7729
Naomi Dushay <ndushay@stanford.edu>
2011-10-03 13:48:46 -0700

    add helper method to create image tag from fedora object and symbol for <resource> element in contentMetadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,7 +27,7 @@ module ApplicationHelper
   end
   
   # get the URL to retrieve the content of a DataStream as indicated in contentMetadata
-  # @param [ActiveFedora::ContentModel] the fedora object
+  # @param [ActiveFedora::ContentModel] the fedora object with the contentMetadata
   # @param [Symbol or String] the filename (or filename field) in the contentMetadata -- id attribute of a <file> element
   # @param [Symbol] the OM term for the pid of the FileAsset Fedora object containing the datastream -- objectId attribute of <resource> element in contentMetadata
   # @param [Symbol] the OM term for the datastream id in the FileAsset Fedora object -- the contents of the <location> element with type attribute=datastreamId
@@ -41,5 +41,16 @@ module ApplicationHelper
     asset_downloads_path(file_asset_pid, :download_id=>file_ds_id)
   end
   
+  # return an image tag populated according to the contentMetadata in the fedora object and the symbol for the term for the specific <resource> element in the contentMetadata
+  #  the filename of the image (used in :alt) is from the term [resource_ref_symbol, :file, :filename]
+  # @param [ActiveFedora::ContentModel] the fedora object with the contentMetadata
+  # @param [Symbol] the term assigned to the desired <resource> element (often chosen with the value of the type attribute, e.g.  t.dd(:ref=>:resource, :attributes=>{:type=>"media-file"}) )
+  # @param [Hash] any additional args to be used in the image tag
+  def get_image_tag_from_content_md(fedora_obj, resource_ref_symbol, addl_args)
+    filename = get_values_from_datastream(fedora_obj, "contentMetadata", [resource_ref_symbol, :file, :filename])
+    url = get_datastream_url_from_content_md(fedora_obj, filename, [resource_ref_symbol, :fedora_pid], [resource_ref_symbol, :file, :ds_id])
+    addl_args[:alt] = filename
+    image_tag(url, addl_args)
+  end
   
 end
