b1f78bbb060a646b0dfc8978a4f52203d5e81767
Naomi Dushay <ndushay@stanford.edu>
2011-10-04 16:36:28 -0700

    added step def for checking the displayed text of a link

--- features/step_definitions/hypatia_steps.rb
+++ features/step_definitions/hypatia_steps.rb
@@ -1,14 +1,19 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
-Then /^I should see a link to the show page for "([^"]*)"$/ do |object_id|
+Then /^I (should|should not) see a link to the show page for "([^"]*)"$/ do |bool, object_id|
   path = "the show page for #{object_id}"
-  page.should have_xpath(".//a[@href=\"#{path_to(path)}\"]")
+  if bool == "should"
+    page.should have_xpath(".//a[@href=\"#{path_to(path)}\"]")
+  else
+    page.should_not have_xpath(".//a[@href=\"#{path_to(path)}\"]")
+  end
 end
 
-Then /^I should not see a link to the show page for "([^"]*)"$/ do |object_id|
+Then /^I should see a link to the show page for "([^"]*)" with label "([^"]*)"$/ do |object_id, link_label|
   path = "the show page for #{object_id}"
-  page.should_not have_xpath(".//a[@href=\"#{path_to(path)}\"]")
+  page.should have_xpath(".//a[@href=\"#{path_to(path)}\"]", :text=>link_label)  
 end
+
 
 # 'I should see "text"' step  with comment at end of line
 Then /^I should see "(.*?)"(?: +\#.*)$/ do |text|
