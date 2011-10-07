31725adeca5bad180d28d885a8ad9de35d2c390b
Jessie Keck <jessie.keck@gmail.com>
2011-10-07 14:23:45 -0700

    Fixing edit feature and config.

--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -29,8 +29,8 @@ if Hydra.respond_to?(:configure)
                                {:name => "permissions",    :edit_partial => "permissions/permissions_form",   :show_partial => "shared/show_permissions"}
                               ],
         :hypatia_ftk_items => [{:name => "description",     :edit_partial => "hypatia_ftk_items/description_form",  :show_partial => "hypatia_ftk_items/show_description"},
-                               {:name => "files",           :edit_partial => "file_assets/file_assets_form",        :show_partial => "shared/show_files"},
-                               {:name => "technical_info",  :edit_partial => "hypatia_ftk_items/tech_info_form",    :show_partial => "shared/show_technical"},
+                               {:name => "files",           :edit_partial => "file_assets/file_assets_form",        :show_partial => "hypatia_ftk_items/show_files"},
+                               {:name => "technical_info",  :edit_partial => "hypatia_ftk_items/tech_info_form",    :show_partial => "hypatia_ftk_items/show_technical"},
                                {:name => "permissions",     :edit_partial => "permissions/permissions_form",        :show_partial => "shared/show_permissions"}
                               ],
         :hypatia_sets =>      [{:name => "description",     :edit_partial => "hypatia_sets/description_form",       :show_partial => "hypatia_sets/show_description"},
--- features/edit_hypatia_items.feature
+++ features/edit_hypatia_items.feature
@@ -11,14 +11,18 @@ Feature: Edit Hypatia items
     
   Scenario: Editing object
     Given I am logged in as the "archivist1" user
-    When I am on the edit files page for hypatia:fixture_xanadu_drive1
+    When I am on the edit files page for hypatia:fixture_ftk_file_item
     Then I should see "Description"
-    And I should see "born digital"
+    And I should see "WordPerfect 4.2"
     And I should see "Number of files add"
-    When I am on the edit technical_info page for hypatia:fixture_xanadu_drive1
-    Then I should see "Content Object ID"
-    And I should see "druid:tk694zs2244"
-    When I am on the edit permissions page for hypatia:fixture_xanadu_drive1
-    Then I should see "Technical info"
+    When I am on the edit technical_info page for hypatia:fixture_ftk_file_item
+    Then I should see "FileAsset Object Id"
+    And I should see "WordPerfect 4.2"
+    When I am on the edit permissions page for hypatia:fixture_ftk_file_item
+    Then I should see "Technical Information"
     And I should see "Group Permissions"
   
+  # Scenario: Saving data in objects
+  #   Given I am logged in as the "archivist1" user
+  #   When I am on the edit document page for hypatia:fixture_xanadu_drive1
+  #   And I fill in "digital_origin" with "born digital"
