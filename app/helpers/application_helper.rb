eb051ab9ca949f0368cb71a3b19c8cce3efb78a1
Naomi Dushay <ndushay@stanford.edu>
2011-10-29 14:45:46 -0700

    removed vestigial helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,16 +6,6 @@ module ApplicationHelper
   def application_name
     'Hypatia'
   end
-  
-  # display the Solr values populated per the datastream model within a fieldset html tag
-  #  this is a local helper method created to avoid all the repeated stuff in hydra-head/app/views/mods_assets/_show_description.html.erb
-  def display_ds_values_as_fieldset(dsid, solr_fld_sym, display_label)
-    values = get_values_from_datastream(@document_fedora, dsid, [solr_fld_sym])
-    unless values.first.empty?
-      result = "<fieldset><legend>#{display_label}</legend><div class=\"browse_value\">#{values.join(', ')}</div></fieldset>"
-    end
-    result 
-  end
 
   # display the Solr values populated per the datastream model with dt and dd html tags
   def display_ds_values_as_dl_element(dsid, solr_fld_sym, display_label)
